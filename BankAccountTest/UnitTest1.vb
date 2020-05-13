f4be48d4f0f21b09e587e7bca50b9c2f0c2c7ebd
171067 <171067@trinityschools.nz>
2020-05-14 10:29:38 +1200

    BankAccounts Testing 1

--- BankAccountTest/UnitTest1.vb
+++ BankAccountTest/UnitTest1.vb
@@ -3,7 +3,24 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
 <TestClass()> Public Class UnitTest1
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub TestConstructors()
+
+        'Arrange 
+        Dim Accountholder As String = "MR S. N. David"
+        Dim AccountNumber As String = "ABCD 890111 11167890"
+        Dim Interestrate As Double = 2.5
+        Dim BankBalance As Double = 200000.8
+        Dim CountryOfOrigin As String = "whoo Vill"
+
+        'ACT
+        Dim Account1 As New BankAccounts.BankAccount(Accountholder, AccountNumber, Interestrate, BankBalance, CountryOfOrigin)
+        Dim Account2 As New BankAccounts.BankAccount(Accountholder, AccountNumber, Interestrate, BankBalance)
+        Dim Account3 As New BankAccounts.BankAccount(Accountholder, AccountNumber, Interestrate,)
+        'Assert
+        Assert.IsNotNull(Account1)
+        Assert.IsNotNull(Account2)
+        Assert.IsNotNull(Account3)
+
     End Sub
 
 End Class
